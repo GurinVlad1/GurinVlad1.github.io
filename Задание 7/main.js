280d4db3af61ee608a2c0f0fb6cdfaea429ed754
GurinVlad1 <144003841+GurinVlad1@users.noreply.github.com>
2023-10-26 18:59:37 +0300

    Add files via upload

--- Задание 7/main.js
+++ Задание 7/main.js
@@ -1,6 +1,19 @@
-$('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
+$(document).ready(function () {
+  $(".slider").slick({
+      arrows: true,
+      dots: true,
+      infinite: true,
+      responsive: [
+          {
+              breakpoint: 770,
+              settings: {
+                  slidesToScroll: 1,
+                  slidesToShow: 1
+              }
+          }
+      ],
+      slidesToScroll: 3,
+      slidesToShow: 3
   });
-  
+});
+
